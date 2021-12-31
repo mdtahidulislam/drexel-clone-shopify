535205e5bee8fd0767020e84e3e8cf64b263650b
mdtahidulislam <tahidul.developer@gmail.com>
2021-12-31 11:28:15 +0600

    add starter theme

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+    /*================================
+        soutcebook tab carousel
+    =================================*/
+    $(document).ready(function(){
+        // $(".sourcebook-active").owlCarousel({
+        //     margin: 15,
+        //     dots: false,
+        //     loop: true,
+        //     nav: true,
+        //     responsive: {
+        //         0: {
+        //             items: 1,
+        //         },
+        //         600: {
+        //             items: 2,
+        //         },
+        //         1000: {
+        //             items: 5,
+        //         }
+        //     }
+        // });
+      });
+})(jQuery);
